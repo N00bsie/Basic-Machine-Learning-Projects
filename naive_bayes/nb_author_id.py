c4ce9dc08a6245e4998bb9c4bfb8c6daf4dde70e
N00bsie <semanticIyer@gmail.com>
2017-07-20 12:01:31 -0400

    Completing the exercise for simple naive bayes in sklearn

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -14,6 +14,7 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
 
 
 ### features_train and features_test are the features for the training
@@ -23,9 +24,19 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 
-
 #########################################################
 ### your code goes here ###
+
+gnb = GaussianNB()
+t0 = time()
+gnb =  gnb.fit(features_train,labels_train)
+print "Training Time : ", round(time()-t0,3),"s"
+
+t1 = time()
+labels_pred = gnb.predict(features_test)
+print "Predicting Time : ",round(time()-t1,3),"s"
+from sklearn.metrics import accuracy_score
+print accuracy_score(labels_test, labels_pred)
 
 
 #########################################################
