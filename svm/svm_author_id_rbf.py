c5afb2c05006f7f9aa06d058da78c5fbd8da669d
N00bsie <semanticIyer@gmail.com>
2017-07-22 17:06:03 -0400

    Finding an optimized value for C and rbf kernel

---
+++ svm/svm_author_id_rbf.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+""" 
+    This is the code to accompany the Lesson 2 (SVM) mini-project.
+
+    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Sara has label 0
+    Chris has label 1
+"""
+    
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+features_train = features_train[:len(features_train)/100]  
+labels_train = labels_train[:len(labels_train)/100]
+
+#########################################################
+
+
+cValues = [10.0,100.0,1000.0,10000.0]
+
+for c in cvalues:
+    from sklearn import svm
+svmclassifier = svm.SVC(c, kernel='rbf')
+t0 = time()
+svmclassifier = svmclassifier.fit(features_train,labels_train)
+print "Training Time : ", round(time()-t0,3),"s"
+
+t1 = time()
+pred = svmclassifier.predict(features_test)
+print "Predicting Time : ",round(time()-t1,3),"s"
+from sklearn.metrics import accuracy_score
+
+print c + " : " + accuracy_score(labels_test, pred)
+    
+
+
+
+
