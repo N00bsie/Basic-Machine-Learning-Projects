ca73a96f607ed9b31219a87d62df21d19f497e58
N00bsie <semanticIyer@gmail.com>
2017-07-22 16:50:13 -0400

    Implementing the SVM classifier out of the box

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,6 +24,18 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+svmclassifier = svm.SVC(kernel='linear')
+t0 = time()
+svmclassifier = svmclassifier.fit(features_train,labels_train)
+print "Training Time : ", round(time()-t0,3),"s"
+
+t1 = time()
+pred = svmclassifier.predict(features_test)
+print "Predicting Time : ",round(time()-t1,3),"s"
+from sklearn.metrics import accuracy_score
+
+print accuracy_score(labels_test, pred,)
 
 #########################################################
 
